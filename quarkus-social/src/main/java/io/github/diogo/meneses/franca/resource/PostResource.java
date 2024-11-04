16d14321a0757ca042c406f1d7cd238107080bc9
Diogo Meneses de Franca <m319726@alunos.ipb.pt>
2024-11-04 15:49:17 -0300

    feat: classe PostResource

---
+++ quarkus-social/src/main/java/io/github/diogo/meneses/franca/resource/PostResource.java
@@ -0,0 +1,31 @@
+package io.github.diogo.meneses.franca.resource;
+
+import io.github.diogo.meneses.franca.dto.CreatePostRequest;
+import io.github.diogo.meneses.franca.service.PostService;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import lombok.RequiredArgsConstructor;
+
+@Path("/posts")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+@RequiredArgsConstructor
+public class PostResource {
+
+	private final PostService service;
+
+	@POST
+	@Path("/{userId}")
+	public Response savePost(@PathParam("userId") Long userId, CreatePostRequest request){
+		return service.savePost(userId, request);
+	}
+
+	@GET
+	@Path("/{userId}")
+	public Response listPosts(@PathParam("userId") Long userId){
+		return Response.status(Response.Status.OK).build();
+	}
+
+
+}

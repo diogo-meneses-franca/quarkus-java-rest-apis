696347dbdfe399a053645d843f2f40571a7e2ee9
Diogo Meneses de Franca <m319726@alunos.ipb.pt>
2024-11-04 15:52:07 -0300

    feat: adicionadas as anotacoes do lombok e o id do objeto

--- quarkus-social/src/main/java/io/github/diogo/meneses/franca/dto/CreateUserRequest.java
+++ quarkus-social/src/main/java/io/github/diogo/meneses/franca/dto/CreateUserRequest.java
@@ -2,8 +2,18 @@ package io.github.diogo.meneses.franca.dto;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class CreateUserRequest {
+
+	private Long id;
 
 	@NotBlank(message = "Name is required!")
 	private String name;
@@ -11,26 +21,4 @@ public class CreateUserRequest {
 	@NotNull(message = "Age is required!")
 	private Integer age;
 
-	public CreateUserRequest(Integer age, String name) {
-		this.age = age;
-		this.name = name;
-	}
-
-	public CreateUserRequest(){}
-
-	public Integer getAge() {
-		return age;
-	}
-
-	public void setAge(Integer age) {
-		this.age = age;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
 }

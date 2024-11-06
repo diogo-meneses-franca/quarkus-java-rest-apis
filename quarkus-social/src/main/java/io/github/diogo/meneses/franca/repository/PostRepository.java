b076680df40fa2a11d7cde4ec18cb481cb98b5ee
Diogo Meneses de Franca <m319726@alunos.ipb.pt>
2024-11-06 13:16:31 -0300

    feat: método findPostByUserSortedByDateTime

--- quarkus-social/src/main/java/io/github/diogo/meneses/franca/repository/PostRepository.java
+++ quarkus-social/src/main/java/io/github/diogo/meneses/franca/repository/PostRepository.java
@@ -1,9 +1,17 @@
 package io.github.diogo.meneses.franca.repository;
 
 import io.github.diogo.meneses.franca.model.Post;
+import io.github.diogo.meneses.franca.model.User;
 import io.quarkus.hibernate.orm.panache.PanacheRepository;
+import io.quarkus.panache.common.Sort;
 import jakarta.enterprise.context.ApplicationScoped;
+
+import java.util.List;
 
 @ApplicationScoped
 public class PostRepository implements PanacheRepository<Post> {
+
+	public List<Post> findPostsByUserSortedByDateTimeDesc(User user) {
+		return find("user", Sort.by("dateTime", Sort.Direction.Descending), user).list();
+	}
 }

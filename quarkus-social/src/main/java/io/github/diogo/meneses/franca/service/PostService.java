372baeefadca5da212fe5baa358c97da6efb1dd7
Diogo Meneses de Franca <m319726@alunos.ipb.pt>
2024-11-06 13:19:45 -0300

    feat: metodo listPosts e createPost retornam Response<PostResponse>

--- quarkus-social/src/main/java/io/github/diogo/meneses/franca/service/PostService.java
+++ quarkus-social/src/main/java/io/github/diogo/meneses/franca/service/PostService.java
@@ -1,6 +1,7 @@
 package io.github.diogo.meneses.franca.service;
 
 import io.github.diogo.meneses.franca.dto.CreatePostRequest;
+import io.github.diogo.meneses.franca.dto.PostResponse;
 import io.github.diogo.meneses.franca.model.Post;
 import io.github.diogo.meneses.franca.model.User;
 import io.github.diogo.meneses.franca.repository.PostRepository;
@@ -8,6 +9,10 @@ import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import jakarta.transaction.Transactional;
 import jakarta.ws.rs.core.Response;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @ApplicationScoped
 public class PostService {
@@ -31,8 +36,20 @@ public class PostService {
 		post.setText(request.getText());
 		post.setUser(user);
 		repository.persist(post);
+		PostResponse response = PostResponse.fromPost(post);
 		return Response.status(Response.Status.CREATED)
-				.entity(post)
+				.entity(response)
+				.build();
+	}
+
+	public Response listPosts(Long userId){
+		User user = userService.findUser(userId);
+		if(user == null) return Response.status(Response.Status.NOT_FOUND).build();
+		List<Post> postList = repository.findPostsByUserSortedByDateTimeDesc(user);
+		List<PostResponse> responseList = PostResponse.fromPostList(postList);
+		return Response
+				.status(Response.Status.OK)
+				.entity(responseList)
 				.build();
 	}
 }
